329ea95f930dd7d591b0ef6b15bce109b4aa313c
trevorroyer <trevorroyer@gmail.com>
2020-01-05 15:19:46 -0800

    building out scraper for different object types

--- scrape.py
+++ scrape.py
@@ -1,17 +1,63 @@
 from py2neo import Graph
 
-from graphmodels import Clan, Player
+from graphmodels import Clan, Player, Card
+import royalerequest
 
 clash = Graph(host = "localhost", auth=("neo4j", "test123"))
 
-rares = Clan()
-rares.tag = "tag123"
-rares.name = "The Blunt Rares"
+def updateCards():
+    cards = royalerequest.getCards(base_url, headers)
 
-test = Player()
-test.tag = "tag234"
-test.name = "Strangiato"
-test.member_of.add(rares)
+    for card in cards:
+        node_card = Card()
+        node_card.key = card["key"]
+        node_card.name = card["name"]
+        node_card.elixir = card["elixir"]
+        node_card.card_type = card["type"]
+        node_card.rarity = card["rarity"]
+        node_card.description = card["description"]
+        clash.push(node_card)
 
-clash.push(rares)
-clash.push(test)
+def updateClan(tag):
+    clan = royalerequest.getClan(base_url, headers, tag)
+
+    node_clan = Clan()
+    node_clan.tag = clan["tag"]
+    node_clan.name = clan["name"]
+    node_clan.description = clan["description"]
+    node_clan.clan_type = clan["type"]
+    node_clan.score = clan["score"]
+    node_clan.warTrophies = clan["warTrophies"]
+    node_clan.memberCount = clan["memberCount"]
+    node_clan.requiredScore = clan["requiredScore"]
+    node_clan.donations = clan["donations"]
+
+    clash.push(node_clan)
+
+    for player in clan["members"]:
+        updatePlayer(player["tag"], node_clan)
+
+def updatePlayer(tag, clan = None):
+    player = royalerequest.getPlayer(base_url, headers, tag)
+
+    node_player = Player()
+    node_player.tag = player["tag"]
+    node_player.name = player["name"]
+    node_player.clan_role = player["clan"]["role"]
+    node_player.trophies = player["trophies"]
+    # node_player.donations = player["clan"]["donations"]
+    # node_player.donationsReceived = player["clan"]["donationsReceived"]
+    # node_player.donationsDelta = player["clan"]["donationsDelta"]
+
+    if clan != None:
+        print(type(clan))
+        node_player.member_of.add(clan)
+
+    clash.push(node_player)
+
+if __name__ == "__main__":
+    base_url = royalerequest.getBaseURL()
+    headers = royalerequest.getHeaders()
+
+    updateCards()
+    updateClan("VV80RJY")
