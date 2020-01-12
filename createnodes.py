ab83da3fe614d39c7a648721c663497aad6851cd
strangiato <trevorroyer@gmail.com>
2020-01-12 16:14:03 -0600

    Add deck creation and fix bugs

--- createnodes.py
+++ createnodes.py
@@ -1,10 +1,13 @@
+from py2neo import Graph
 from graphmodels import Clan, Player, Card, Deck, Battle, BattleTeam
+
+def getGraph():
+    return Graph(host = "localhost", auth=("neo4j", "test123"))
 
 def createCard(graph, key, name, elixir, card_type, rarity, description):
     card_node = Card.match(graph, key).first()
 
-    if card_node is not None:
-    
+    if card_node is None:
         card_node = Card()
         card_node.key = key
         card_node.name = name
@@ -19,7 +22,7 @@ def createCard(graph, key, name, elixir, card_type, rarity, description):
 def createClan(graph, tag, name, description = None, clan_type = None, score = None, war_trophies = None, member_count = None, required_score = None, donations = None):
     clan_node = Clan.match(graph, tag).first()
 
-    if clan_node is not None:
+    if clan_node is None:
         clan_node = Clan()
         clan_node.tag = tag
         clan_node.name = name
@@ -39,13 +42,15 @@ def createPlayer(graph, tag, name, trophies = None, clan_role = None, clan_node 
 
     player_node = Player.match(graph, tag).first()
 
-    if player_node is not None:
+    if player_node is None:
         player_node = Player()
         player_node.tag = tag
         player_node.name = name
         player_node.trophies = trophies
         player_node.clan_role = clan_role
 
+        # does this need to be outside of the if block
+        # possible issue when player is created then joins clan
         if clan_node is not None:
             assert(isinstance(clan_node, Clan))
             player_node.member_of.add(clan_node)
@@ -54,3 +59,23 @@ def createPlayer(graph, tag, name, trophies = None, clan_role = None, clan_node 
 
     return player_node
 
+def createDeck(graph, deck):
+    # todo: 
+    # find way to better check to see if the deck already exists
+    # if it does already exist skip the card lookup and return the existing node
+
+    #deck_node = Deck.match(graph, Deck.deckHash(deck)).first()
+
+    #if deck_node is None:
+    deck_node = Deck()
+    deck_node.deckHash(deck)
+
+    for card in deck:
+        node_card = Card.match(graph, card["key"]).first()
+        deck_node.contains.add(node_card)
+
+    deck_node.calculateExilir(deck)
+
+    graph.merge(deck_node)
+
+    return deck_node
