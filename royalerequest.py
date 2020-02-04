57c6323768fc0d2ba4912efc58ee1d8be454a379
strangiato <trevorroyer@gmail.com>
2020-02-03 22:31:09 -0600

    Fix logging levels

--- royalerequest.py
+++ royalerequest.py
@@ -4,7 +4,7 @@ import json
 import logging
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.WARNING)
+logger.setLevel(logging.INFO)
 
 formatter = logging.Formatter(
     '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
